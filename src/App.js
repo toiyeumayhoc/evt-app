366ad4104d999991ebff789fc33f08b7e0088979
Admin <icanlearnvi@gmail.com>
2023-01-03 22:18:05 +0700

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button,TextField} from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [job, setJob] = useState('');
+  const handleSubmit = () => {
+    setJobs(prev => [...prev,job]);
+    setJob('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" value={job}
+        onChange={e => setJob(e.target.value)} />
+        <Button variant="contained" onClick={handleSubmit}>Add</Button>
+      </div>
+      <div>
+        <ul>
+          {jobs.map((job,index) => {
+            return <li key={index}>{job}</li>
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
